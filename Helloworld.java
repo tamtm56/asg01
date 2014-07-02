a416c63a716eeec5bbd4386e64b3ae005876a09e
tamtm56 <tamtm_56@vnu.edu.vn>
2014-07-02 18:04:14 +0700

    Helloworld
    First work on oop class

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Trinh Minh Tam \t");
+		System.out.print("11020274 \t");
+		System.out.print("K56CC \t");
+		System.out.print("tamtm56 \t");
+		System.out.print("tamtm_56@vnu.edu.vn \t");
+	}
+}
