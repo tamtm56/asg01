490ea3a5da238137880f6c93c4ecee7a8e3e6084
tamtm56 <tamtm_56@vnu.edu.vn>
2014-07-03 20:31:28 +0700

    Java code for asg01
    Lastest Beer.java

---
+++ Beer.java
@@ -0,0 +1,17 @@
+public class Beer
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Trinh Minh Tam \t");
+		System.out.print("11020274 \t");
+		System.out.print("K56CC \t");
+		System.out.print("tamtm56 \t");
+		System.out.print("tamtm_56@vnu.edu.vn \n \n");
+		for (int i=9;i>0;i--)
+		{
+			System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer.");
+			System.out.println("Take one down, pass it around,");
+		}
+		System.out.print("No more bottles of beer on the wall.");
+	}
+}
